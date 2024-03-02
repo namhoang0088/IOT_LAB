65d5874810a59b4cc085dab146aa32b47ee71f29
HoangNam <huyquang0081@gmail.com>
2024-03-02 15:33:39 +0700

    lab2

---
+++ Lab2_2/uart.py
@@ -0,0 +1,76 @@
+import serial.tools.list_ports
+import random
+import time
+import  sys
+from  Adafruit_IO import  MQTTClient
+
+AIO_FEED_ID = ["led","temperature","log"]
+AIO_USERNAME = "huyquang0081"
+AIO_KEY = "aio_qEtD47qyP9ZprNXfILlQ3Ib1L8V2"
+
+def  connected(client):
+    print("Ket noi thanh cong...")
+    client.subscribe(AIO_FEED_ID)
+
+def  subscribe(client , userdata , mid , granted_qos):
+    print("Subcribe thanh cong...")
+
+def  disconnected(client):
+    print("Ngat ket noi...")
+    sys.exit (1)
+
+def  message(client , feed_id , payload):
+    print("Nhan du lieu: " + payload)
+    ser.write((str(payload) + "#").encode())
+
+client = MQTTClient(AIO_USERNAME , AIO_KEY)
+client.on_connect = connected
+client.on_disconnect = disconnected
+client.on_message = message
+client.on_subscribe = subscribe
+client.connect()
+client.loop_background()
+
+def getPort():
+    ports = serial.tools.list_ports.comports()
+    N = len(ports)
+    commPort = "None"
+    for i in range(0, N):
+        port = ports[i]
+        strPort = str(port)
+        if "USB Serial Device" in strPort:
+            splitPort = strPort.split(" ")
+            commPort = (splitPort[0])
+    return "COM9"
+
+if(getPort() != "None"):
+    ser = serial.Serial( port=getPort(), baudrate=115200)
+    print(ser)
+
+mess = ""
+def processData(client, data):
+    data = data.replace("!", "")
+    data = data.replace("#", "")
+    splitData = data.split(":")
+    print(splitData)
+    if splitData[1] == "TEMP":
+        client.publish("temperature", splitData[2])
+
+mess = ""
+def readSerial(client):
+    bytesToRead = ser.inWaiting()
+    if (bytesToRead > 0):
+        global mess
+        mess = mess + ser.read(bytesToRead).decode("UTF-8")
+        while ("#" in mess) and ("!" in mess):
+            start = mess.find("!")
+            end = mess.find("#")
+            processData(client, mess[start:end + 1])
+            if (end == len(mess)):
+                mess = ""
+            else:
+                mess = mess[end+1:]
+
+while True:
+    readSerial(client)
+    time.sleep(1)
